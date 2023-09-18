de5273d534b38f01c76e0c770b60d95a5c57feee
Kapskii <kapskij20@gmail.com>
2023-09-18 16:06:17 +0300

    city

---
+++ lesson-02/06.js
@@ -0,0 +1,20 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let passportWithAddress1 = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "Belarus",
+        city: "Bobryisk"
+    }
+};
+
+console.log(passportWithAddress.address.city);
+console.log(passportWithAddress1.address.city);
