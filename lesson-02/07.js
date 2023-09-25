7cf6bd8527a6cc518363b794d4be95bc10982f45
Kapskii <kapskij20@gmail.com>
2023-09-25 09:49:48 +0300

    arr

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -8,8 +8,26 @@ let passportMarried = {
 };
 
 let passportMarried2 = {
-    ...passportMarried, married: true, address: {...passportMarried.address}
+    ...passportMarried,
+    married: true,
+    address: {
+        ...passportMarried.address
+    }
 };
-passportMarried2.married=true
+passportMarried2.married = true
 console.log(passportMarried);
 console.log(passportMarried2);
+
+
+
+let pow = (x,n) => {
+    let result = x**n;
+    console.log(result);
+    return result
+}
+
+
+
+
+pow(3, 2) // 9
+pow(3, 3) //27
--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,7 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString(arr) {
+    return arr.filter((elem) => elem === 'черный' || elem === 'красный' || elem === 'желтый').join('-')
+}
+console.log(createColorString(colors));
+
