16313c1071b32e3ce736cf9d4d1827898327291d
Kapskii <kapskij20@gmail.com>
2023-09-25 11:54:39 +0300

    fix

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -17,17 +17,3 @@ let passportMarried2 = {
 passportMarried2.married = true
 console.log(passportMarried);
 console.log(passportMarried2);
-
-
-
-let pow = (x,n) => {
-    let result = x**n;
-    console.log(result);
-    return result
-}
-
-
-
-
-pow(3, 2) // 9
-pow(3, 3) //27
