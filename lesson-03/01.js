7cdf1aded038a9cead94f6fbee97e0eb470fd7c6
Kapskii <kapskij20@gmail.com>
2023-09-25 10:50:20 +0300

    fixBug

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,7 +1,7 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    return colors.filter((elem) => elem === 'черный' || elem === 'красный' || elem === 'желтый').join('-');
+    return colors.filter((elem) => elem === "черный" || elem === "красный" || elem === "желтый").join('-');
 }
 
 console.log(createColorString());
